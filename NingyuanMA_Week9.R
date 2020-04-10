6c54e1c6395b4e7ccbb6352e51f4db60e2c14368
Ning-Yuan Ma <mrmaningyuan@163.com>
2020-04-10 11:54:11 -0400

    Building model using package gbm

--- NingyuanMA_Week9.R
+++ NingyuanMA_Week9.R
@@ -1,4 +1,9 @@
 Sys.setlocale("LC_ALL", "English")
+library(naniar)
+library(dataQualityR)
+library(gbm)
+library(caret)
+
 path_train <- "D:/G-OneDrive/OneDrive/1-NYU/2-Business Analytics/2-Homework/Week 9(project 2)/Project 2 External -S20/application_train_S20.csv"
 path_toScore <- "D:/G-OneDrive/OneDrive/1-NYU/2-Business Analytics/2-Homework/Week 9(project 2)/Project 2 External -S20/applications_to_score_S20.csv"
 path_cardBalance <- "D:/G-OneDrive/OneDrive/1-NYU/2-Business Analytics/2-Homework/Week 9(project 2)/Project 2 External -S20/credit_card_balance.csv"
@@ -26,13 +31,13 @@ for( i in 1:length(names(df_cardBalance)))
     print("False")
 }
 
-df_train <- read.csv("~/Downloads/df_train_S20.csv")
+
 head(df_train)
 
-library(naniar)
-vis_miss(df_train, warn_large_data=FALSE)
+# Doesn't work
+# vis_miss(df_train, warn_large_data=FALSE)
+# 
 
-library(dataQualityR)
 checkDataQuality(df_train, 
                  out.file.num ="~/Downloads/dq_train_num.csv", 
                  out.file.cat= "~/Downloads/dq_train_cat.csv")
@@ -42,13 +47,14 @@ View(dq_num)
 dq_cat<-read.csv("~/Downloads/dq_train_cat.csv")
 View(dq_cat)
 
+# Removing columns with more than 20% empty values
 missing_percentage<-apply(df_train, 2, function(col)sum(is.na(col))/length(col))
 missing_percentage[missing_percentage>0.2]
 missing_percentage_df<-as.data.frame(missing_percentage[missing_percentage>0.2])
 index<-rownames(missing_percentage_df)
 df_train_2<- df_train[,!(names(df_train) %in% index)]
 
-
+# Removing columns with p-value lower than 0.05 in chi square test 
 correlation_percentage<-apply(df_train_2, 2, function(col)chisq.test(df_train_2$TARGET,col)$p.value)
 correlation_percentage
 
@@ -59,6 +65,7 @@ index_2<-c(index,"TARGET")
 df_train_3<- df_train_2[,(names(df_train_2) %in% index_2)]
 colnames(df_train_3)
 
+# Reformatting some columns
 df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR<-as.character(df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR)
 df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new<-ifelse(  is.na(df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR),"NULL",df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR)
 df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new<-as.factor(df_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new)
@@ -77,4 +84,13 @@ table(df_train_3$AMT_REQ_CREDIT_BUREAU_WEEK.new)
 to.remove <-c("AMT_REQ_CREDIT_BUREAU_HOUR","AMT_REQ_CREDIT_BUREAU_DAY","AMT_REQ_CREDIT_BUREAU_WEEK")
 df_train_4 <- df_train_3[,-which(names(df_train_3) %in% to.remove)]
 df_train_4  <- na.omit(df_train_4)
-modelLookup()
+
+df_train_4$TARGET
+gbm_1 <- gbm(formula=TARGET~.,
+             distribution = "bernoulli",
+             data=df_train_4,
+             n.trees = 1000)
+
+
+# which package? What for?
+# modelLookup()
