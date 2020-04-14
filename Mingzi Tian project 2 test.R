a2eefb7062f815062224d7488c089dc63e338166
Mingzi Tian <63325342+unknownt62@users.noreply.github.com>
2020-04-14 07:57:37 -0400

    update 7:57

--- Mingzi Tian project 2 test.R
+++ Mingzi Tian project 2 test.R
@@ -13,9 +13,8 @@ library(dplyr)
 library(MLmetrics)
 library(ROSE)
 library(pROC)
+library(DMwR)
 #######################################
-
-
 application_train <- read.csv("/Users/tian/Desktop/Business Analytics/BA Project 2/application_train_S20.csv")
 
 missing_percentage<-apply(application_train, 2, function(col)sum(is.na(col))/length(col))
@@ -24,44 +23,53 @@ missing_percentage[missing_percentage>0.2]
 missing_percentage_df<-as.data.frame(missing_percentage[missing_percentage>0.2])
 index<-rownames(missing_percentage_df)
 application_train_2<- application_train[,!(names(application_train) %in% index)]
-
-correlation_percentage<-apply(application_train, 2, function(col)chisq.test(application_train$TARGET,col,simulate.p.value=TRUE)$p.value)
+application_train_2[,c(2,22:27,34:39,52:71)]<- lapply(application_train_2[,c(2,22:27,34:39,52:71)], as.factor)
+str(application_train_2)
+correlation_percentage<-apply(application_train_2[,sapply(application_train_2, is.factor)], 2, 
+                              function(col)chisq.test(application_train_2$TARGET,col,simulate.p.value=TRUE)$p.value)
 
 correlation_percentage[correlation_percentage>0.05]
 correlation_df<-as.data.frame(correlation_percentage[correlation_percentage>0.05])
 index<-rownames(correlation_df)
 index_2<-c(index,"TARGET")
-application_train_3<- application_train_2[,(names(application_train_2) %in% index_2)]
-colnames(application_train_3)
 
-application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR<-as.character(application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR)
-application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new<-ifelse(  is.na(application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR),"NULL",application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR)
-application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new<-as.factor(application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new)
-table(application_train_3$AMT_REQ_CREDIT_BUREAU_HOUR.new)
+cor_coefficient <- as.data.frame(cor(application_train_2[sapply(application_train_2, is.numeric)], use="complete.obs"))
+correlation_index <- findCorrelation(cor(cor_coefficient),cutoff=0.9)
+correlation_index
+sum(is.na(application_train_2$AMT_CREDIT)) > sum(is.na(application_train_2$AMT_GOODS_PRICE)) #FALSE, so remove right one
+application_train_2<-subset(application_train_2, select=-AMT_GOODS_PRICE)
 
-application_train_3$AMT_REQ_CREDIT_BUREAU_DAY<-as.character(application_train_3$AMT_REQ_CREDIT_BUREAU_DAY)
-application_train_3$AMT_REQ_CREDIT_BUREAU_DAY.new<-ifelse(  is.na(application_train_3$AMT_REQ_CREDIT_BUREAU_DAY),"NULL",application_train_3$AMT_REQ_CREDIT_BUREAU_DAY)
-application_train_3$AMT_REQ_CREDIT_BUREAU_DAY.new<-as.factor(application_train_3$AMT_REQ_CREDIT_BUREAU_DAY.new)
-table(application_train_3$AMT_REQ_CREDIT_BUREAU_DAY.new)
 
-application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK<-as.character(application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK)
-application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK.new<-ifelse(  is.na(application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK),"NULL",application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK)
-application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK.new<-as.factor(application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK.new)
-table(application_train_3$AMT_REQ_CREDIT_BUREAU_WEEK.new)
+application_train_3<- cbind(application_train_2[,(names(application_train_2) %in% index_2)],
+                            application_train_2[,sapply(application_train_2, is.numeric)])
+str(application_train_3)
 
-to.remove <-c("AMT_REQ_CREDIT_BUREAU_HOUR","AMT_REQ_CREDIT_BUREAU_DAY","AMT_REQ_CREDIT_BUREAU_WEEK")
-application_train_4 <- application_train_3[,-which(names(application_train_3) %in% to.remove)]
-application_train_4  <- na.omit(application_train_4) 
-application_train_4[,c(2,6:17)] <- lapply(application_train_4[,c(2,6:17)], as.factor)
-
-str(application_train_4)
+columnNumber <- which(colnames(application_train_3)=="SK_ID_CURR")
+application_train_3 <- application_train_3[,c(columnNumber,1:ncol(application_train_3)-1)]
+#Using knn imputation to imputate data instead of deleting (time consuming)
+#application_train_4  <- knnImputation(application_train_3, k=5, meth="weighAvg")
+#Using median imputation instead
+Median_Imp <- function(data=data){
+  for(i in 1:ncol(data)){        
+    if(class(data[,i]) %in% c("numeric","integer")){
+      if(sum(is.na(data[,i]))){
+        data[is.na(data[,i]),i] <- 
+          median(data[,i],na.rm = TRUE)
+      }
+    }
+  }
+  return(data)
+}
+application_train_4 <- Median_Imp(application_train_3)
 summary(application_train_4)
-remove(correlation_df)
-remove(missing_percentage_df)
-remove(application_train_2)
+table(application_train_3$TARGET)
+
+rm(correlation_df, missing_percentage_df, application_train_2, index, index_2)
+
+
 
 ###############################################
-#define a removing outlier
+#define a removing outlier function
 remove_outliers <- function(x, na.rm = TRUE, ...) {
   qnt <- quantile(x, probs=c(.25, .75), na.rm = na.rm, ...)
   H <- 1.5 * IQR(x, na.rm = na.rm)
@@ -91,12 +99,12 @@ str(cc_balance)
 ifelse(any(cc_balance$SK_ID_PREV == cc_balance$SK_ID_CURR), "TRUE","FALSE")
 
 #To process numeric data, using mean value for variables for different id
-cc_num <- cc_balance[,c(-1,-21)] %>% group_by(SK_ID_CURR) %>% summarise_all(list(mean))
+cc_num <- cc_balance[,c(-21)] %>% group_by(SK_ID_CURR) %>% summarise_all(list(mean))
 cc_num[,-1] <- remove_all_outliers(cc_num[,-1])
 cor_coefficient <- as.data.frame(cor(cc_num[sapply(cc_num, is.numeric)], use="complete.obs"))
-cc_num<-subset(cc_num, select=-c(CNT_DRAWINGS_OTHER_CURRENT, AMT_DRAWINGS_OTHER_CURRENT, AMT_INST_MIN_REGULARITY,
-                                 AMT_TOTAL_RECEIVABLE,AMT_RECIVABLE, AMT_RECEIVABLE_PRINCIPAL,
-                                 AMT_PAYMENT_CURRENT, SK_DPD, SK_DPD_DEF))
+cor_index <- findCorrelation(cor(cor_coefficient),cutoff=0.9)
+cor_index
+cc_num<-subset(cc_num, select=-c(AMT_DRAWINGS_OTHER_CURRENT))
 
 #To process char data, count frequency
 cc_char <- as.data.frame.matrix(table(cc_balance$SK_ID_CURR,cc_balance$NAME_CONTRACT_STATUS))
@@ -107,11 +115,13 @@ names(cc_char)[1]<- "SK_ID_CURR"
 cc_balance_clean <- merge(cc_num, cc_char, all=TRUE, by="SK_ID_CURR")
 summary(cc_balance_clean)
 
-#merge sets, append to application_train_4, from column 21
-application_train_5 <- merge(application_train, cc_balance_clean,
-                             all.x=TRUE, by="SK_ID_CURR")
-
-str(application_train_5)
+missing_percentage<-apply(cc_balance_clean, 2, function(col)sum(is.na(col))/length(col))
+missing_percentage
+missing_percentage[missing_percentage>0.2]
+missing_percentage_df<-as.data.frame(missing_percentage[missing_percentage>0.2])
+index<-rownames(missing_percentage_df)
+cc_balance_clean<- cc_balance_clean[,!(names(cc_balance_clean) %in% index)]
+str(cc_balance_clean)
 
 ########################################################
 ########################################################
@@ -121,43 +131,34 @@ str(application_train_5)
 
 pre_application <- read.csv("/Users/tian/Desktop/Business Analytics/BA Project 2/previous_application.csv",
                             header=TRUE, stringsAsFactors=FALSE)
+application_to_score <- read.csv("/Users/tian/Desktop/Business Analytics/BA Project 2/applications_to_score_S20.csv",
+                            header=TRUE, stringsAsFactors=FALSE)
 str(pre_application)
 
-#check any other repetitive variables (only SK_ID_CURR)
-colnames(pre_application)[which(colnames(pre_application) %in% colnames(application_train))]
+table(pre_application$NAME_CONTRACT_STATUS)
+pre_var <- pre_application[pre_application$NAME_CONTRACT_STATUS == "Approved" | pre_application$NAME_CONTRACT_STATUS == "Refused",]
+pre_var <- cbind("SK_ID_PREV"=pre_var[,1],pre_var[,colnames(pre_var) %in% colnames(application_to_score)])
+str(pre_var)
+pre_var <- droplevels(pre_var)
 
-#delete column with higher than 0.8 missing percentage
-missing_percentage <- apply(pre_application, 2, function(col)sum(is.na(col))/length(col))
-missing_percentage_df<-as.data.frame(missing_percentage[missing_percentage>0.8])
-index<-rownames(missing_percentage_df)
-pre_application<- pre_application[,!(names(pre_application) %in% index)]
 
-for(i in 3:ncol(pre_application)){
-    if(class(pre_application[,i]) == "character"){
-      pre_application[,i] <- as.factor(pre_application[,i])}
-      else {pre_application[,i] <- as.numeric(pre_application[,i])}
-}
-
-pre_num <- pre_application[,sapply(pre_application, is.numeric)][,-1]
-pre_num <- pre_num %>% group_by(SK_ID_CURR) %>% summarise_all(list(mean))
-str(pre_num)
-
-cor_coefficient <- as.data.frame(cor(pre_num[sapply(pre_num, is.numeric)], use="complete.obs"))
+cor_coefficient <- as.data.frame(cor(pre_var[sapply(pre_var, is.numeric)], use="complete.obs"))
 cor_index <- findCorrelation(cor(cor_coefficient),cutoff=0.9)
 cor_index
+sum(is.na(pre_var$AMT_GOODS_PRICE)) > sum(is.na(pre_var$AMT_CREDIT)) #TRUE,delete the left column
+pre_var<-subset(pre_var, select=-AMT_GOODS_PRICE)
 
-#cut off variables with correlation > 0.9, delete the one with fewer NA values
-sum(is.na(pre_num$AMT_APPLICATION))
-sum(is.na(pre_num$AMT_CREDIT))
-sum(is.na(pre_num$AMT_GOODS_PRICE))
-pre_num<-subset(pre_num, select=-c(AMT_GOODS_PRICE, AMT_CREDIT)) #remove it
-sum(is.na(pre_num$DAYS_TERMINATION))
-sum(is.na(pre_num$DAYS_LAST_DUE))
-pre_num<-subset(pre_num, select=-DAYS_TERMINATION) #remove it
+for(i in 3:ncol(pre_var)){
+    if(class(pre_var[,i]) == "character"){
+      pre_var[,i] <- as.factor(pre_var[,i])}
+      else {pre_var[,i] <- as.numeric(pre_var[,i])}
+}
+
+pre_num <- pre_var[,sapply(pre_var, is.numeric)]
+pre_num <- pre_num[,-1] %>% group_by(SK_ID_CURR) %>% summarise_all(list(mean))
 str(pre_num)
 
-pre_factor <- sapply(pre_application, is.factor)
-pre_factor <- cbind("SK_ID_CURR"=pre_application[,2], pre_application[,pre_factor])
+pre_factor <- cbind("SK_ID_CURR"=pre_var[,2],pre_var[,sapply(pre_var, is.factor)])
 
 dfList <- list()
 for(i in 2:ncol(pre_factor)){
@@ -176,31 +177,49 @@ pre_count <- data.frame("SK_ID_CURR"=row.names(pre_count), pre_count, row.names=
 str(pre_count)
 pre_application_clean <- merge(pre_count, pre_num, all=TRUE, by="SK_ID_CURR")
 
-#Merge application_train_5 and previous application dataset
-application_train_6 <- merge(application_train_5, pre_application_clean, all.x=TRUE, by="SK_ID_CURR")
+
+############################################################
+#Final merge, Merge application_train_5 and previous application dataset
+application_train_5 <- merge(application_train_4, cc_balance_clean,
+                             all.x=TRUE, by="SK_ID_CURR")
+application_train_6 <- merge(application_train_5, pre_application_clean, 
+                             all.x=TRUE, 
+                             by="SK_ID_CURR",
+                             row.names=NULL)
 table(application_train_6$TARGET)
+
 str(application_train_6)
+summary(application_train_6)
+
+#Optional
+#for(i in 2:ncol(application_train_6)){
+#  if(class(application_train_6[,i]) == "integer"){
+#    application_train_6[,i] <- as.factor(application_train_6[,i])}
+#  else {application_train_6[,i] <- as.numeric(application_train_6[,i])}
+#}
+
+#to use complete cases in application_train_6 to build model
+#test<- application_train_6[!complete.cases(application_train_6),]
 
 #save application train 6
-write.csv(application_train_6, file = "application_train_6.csv", quote = FALSE, row.names = FALSE)
-write.csv(pre_application_clean, file = "pre_application_clean.csv", quote = FALSE, row.names = FALSE)
-write.csv(cc_balance_clean, file = "cc_balance_clean.csv", quote = FALSE, row.names = FALSE)
-str(application_train_6)
+write.csv(application_train_6, file = "application_train_6_update.csv", quote = FALSE, row.names = FALSE)
+write.csv(pre_application_clean, file = "pre_application_clean_update.csv", quote = FALSE, row.names = FALSE)
+write.csv(cc_balance_clean, file = "cc_balance_clean.csv_update", quote = FALSE, row.names = FALSE)
 
-                            
+
 ######################################
 ######################################
+
 set.seed(1)
 split <- (.7)
-index <- createDataPartition(application_train_4$TARGET, p=split, list=FALSE)
+index <- createDataPartition(test$TARGET, p=split, list=FALSE)
 
-train.df <- application_train_4[index,]
-test.df <- application_train_4[-index,]
+train.df <- test[index,]
+test.df <- test[-index,]
 
-start.time <- Sys.time()
-train.df.smote <- SMOTE(TARGET ~ ., train.df
-                             ,k = 5
-                             ,perc.over=500,perc.under=200)
+train.df2 <- application_train[index,]
+test.df2 <- application_train[-index,]
+
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
@@ -216,14 +235,14 @@ remove(application_train_3)
 train.df.balanced<-ovun.sample(TARGET~., data = train.df, p=0.40, N= 50000)$data # this runs!
 prop.table(table(train.df.balanced$TARGET))
 
-
 ###########################
 # RF Model
 ###########################
 fitControl <- trainControl(method = "none")
+str(train.df)
 
 start.time <- Sys.time()
-rf_1 <-train(train.df.smote[,3:20],train.df.smote[,2],
+rf_1 <-train(train.df.balanced[,-1:-2],train.df.balanced[,2],
           method='rf',
           trControl=fitControl)
 end.time <- Sys.time()
@@ -249,11 +268,11 @@ legend("bottomright", legend=c("rf"), col=c("red"), lwd=2)
 ################################
 modelLookup(model="rf")
 fitControl <- trainControl(method="repeatedcv", number=5, repeats=3)
-mtry <- sqrt(ncol(train.df.balanced[,3:20]))
+mtry <- sqrt(ncol(train.df.balanced[,-1:-2]))
 tunegrid <- expand.grid(.mtry=mtry)
 
 start.time <- Sys.time()
-rf_2 <-train(train.df.balanced[,3:20], 
+rf_2 <-train(train.df.balanced[,-1:-2], 
           train.df.balanced[,2],
           method='rf',
           tuneGrid=tunegrid,
@@ -279,18 +298,18 @@ legend("bottomright", legend=c("rf","rf tuned"), col=c("red","orange"), lwd=2)
 ###########################################
 #Advanced GBM Tunning
 ####################  create pipeline, grid search and model
-fitControl.gbm3 <- trainControl(method = "repeatedcv",
+fitControl.gbm <- trainControl(method = "repeatedcv",
                                 number = 10,
                                 repeats = 3)
 
 gbm.grid <- expand.grid(interaction.depth = c(3,4,5), 
                         n.trees = 5000, 
-                        shrinkage = 0.05,
-                        n.minobsinnode = 30)
+                        shrinkage = 0.01,
+                        n.minobsinnode = 20)
 
-gbm3.tuned<-train(train.df.balanced[,3:20],train.df.balanced[,2],
+gbm.tuned<-train(train.df.balanced[,-1:-2],train.df.balanced[,2],
                   method='gbm',
-                  trControl=fitControl.gbm3,
+                  trControl=fitControl.gbm,
                   tuneGrid = gbm.grid)
 #################### output the prediction and see confustion matrix and F-1
 gbm.tuned.predict<-predict(gbm3.tuned,test.df[,3:20],type="raw")
